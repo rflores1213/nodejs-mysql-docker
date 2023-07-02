2cc4f453bdc0470745c0531dfe8774040702db52
rflores1213 <ricardo.flores.tyc@gmail.com>
2023-07-02 07:10:32 -0600

    2 commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import { createPool } from 'mysql2/promise'
 
-const app= express()
+const app = express()
 
-app.get('/',(req,res)=> {
+const pool = createPool({
+    host: 'localhost',
+    user: 'ricardo',
+    password: "gk4atia3",
+    port: 3307
+})
+
+app.get('/',(req,res) => {
     res.send('Hello world')
+})
+
+app.get('/ping', async (req,res) => {
+   const result = await pool.query('SELECT NOW()')
+    res.json(result[0])
 })
 
 app.listen(3000)
